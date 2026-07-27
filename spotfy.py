c25a734be5acf97d8c4f3b2ca73438463e05579b
joaovictoraparecido2008-cmyk <joaovictoraparecido2008@gmail.com>
2026-07-27 10:19:00 -0300

    caminho está quase acabado

--- spotfy.py
+++ spotfy.py
@@ -1,8 +1,17 @@
-#automação spotify
 #bibliotecas
-#funcao
+import pyautogui
+import time
+import subprocess
 #pergunta
+musica = input("Qual musica voce quer pesquisar? ")
 #abrir spotify
+subprocess.run("spotify", shell=True)
 #presquisar musica
+time.sleep(8)
+pyautogui.hotkey('ctrl', 'l')
+pyautogui.write(musica)
+pyautogui.press('enter')
 #clicar 
+time.sleep(3)
+
 #fechar aba
