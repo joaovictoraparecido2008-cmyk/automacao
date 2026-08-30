9576ae9d0358427564347853d048c38b69ccd751
joaovictoraparecido2008-cmyk <joaovictoraparecido2008@gmail.com>
2026-08-30 08:27:39 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,36 @@
-import pyautogui
-import time
+from ollama import chat
+from apps_automatizados.spotfy import pesquisar_musica
 
-time.sleep(3)
-a=  pyautogui.position()
-print(a)
+MODELO = "llama3.2"
+
+
+def perguntar_ollama(texto):
+    resposta = chat(
+        model=MODELO,
+        messages=[
+            {
+                "role": "user",
+                "content": texto
+            }
+        ]
+    )
+
+    return resposta["message"]["content"]
+
+
+print("Assistente Ollama iniciado!")
+print("Digite 'sair' para fechar.\n")
+
+while True:
+    comando = input("Você: ")
+
+    if comando.lower() == "sair":
+        print("Encerrando...")
+        break
+
+    resposta = perguntar_ollama(comando)
+
+    print("\nOllama:", resposta)
+    print()
+    if comando == 'spotify' or 'sp':
+        pesquisar_musica()
